1aa4128a1552f7c38c8ead5a26deba7cc9b40f3a
apfesta <andy_festa@yahoo.com>
2021-02-06 16:01:43 -0500

    oembed

--- src/main/java/net/mixednutz/app/server/manager/ApiElementConverter.java
+++ src/main/java/net/mixednutz/app/server/manager/ApiElementConverter.java
@@ -1,13 +1,22 @@
 package net.mixednutz.app.server.manager;
 
+import org.springframework.security.core.Authentication;
+
 import net.mixednutz.app.server.entity.InternalTimelineElement;
+import net.mixednutz.app.server.entity.Oembeds;
 import net.mixednutz.app.server.entity.User;
 
 public interface ApiElementConverter<Entity> {
 
 	InternalTimelineElement toTimelineElement(
 			InternalTimelineElement element, Entity entity, User viewer);
-	
+		
 	boolean canConvert(Class<?> entityClazz);
+	
+	Oembeds.Oembed toOembed(
+			String path, Integer maxwidth, Integer maxheight, String format, 
+			Authentication auth);
+	
+	boolean canConvertOembed(String path);
 	
 }
--- src/main/java/net/mixednutz/app/server/manager/ApiManager.java
+++ src/main/java/net/mixednutz/app/server/manager/ApiManager.java
@@ -1,10 +1,15 @@
 package net.mixednutz.app.server.manager;
 
+import java.util.Optional;
+
+import org.springframework.security.core.Authentication;
+
 import net.mixednutz.api.model.IUser;
 import net.mixednutz.api.model.IUserSmall;
 import net.mixednutz.app.server.entity.InternalTimelineElement;
 import net.mixednutz.app.server.entity.User;
 import net.mixednutz.app.server.entity.UserProfile;
+import net.mixednutz.app.server.entity.Oembeds.Oembed;
 
 public interface ApiManager {
 	
@@ -15,5 +20,8 @@ public interface ApiManager {
 	public IUserSmall toUser(User entity);
 	
 	public IUser toUser(User entity, UserProfile profile);
+	
+	public Optional<Oembed> toOembed(String path, Integer maxwidth, Integer maxheight, String format, 
+			Authentication auth);
 	
 }
