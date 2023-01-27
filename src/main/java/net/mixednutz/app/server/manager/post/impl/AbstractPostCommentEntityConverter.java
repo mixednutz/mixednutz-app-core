e7ee603d5c174edc88ddcb3b9f0c4bd4d7ada81d
Andy <apfes1@gmail.com>
2023-01-27 09:45:06 -0500

    savepoint from weeks ago

---
+++ src/main/java/net/mixednutz/app/server/manager/post/impl/AbstractPostCommentEntityConverter.java
@@ -0,0 +1,35 @@
+package net.mixednutz.app.server.manager.post.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import net.mixednutz.api.core.model.NetworkInfo;
+import net.mixednutz.app.server.entity.InternalTimelineElement;
+import net.mixednutz.app.server.entity.InternalTimelineElement.Type;
+import net.mixednutz.app.server.entity.User;
+import net.mixednutz.app.server.entity.post.AbstractPostComment;
+import net.mixednutz.app.server.manager.ApiElementConverter;
+
+public abstract class AbstractPostCommentEntityConverter<C extends AbstractPostComment> implements ApiElementConverter<C>{
+
+	@Autowired
+	protected NetworkInfo networkInfo;
+	
+	@Override
+	public InternalTimelineElement toTimelineElement(InternalTimelineElement api, C entity, User viewer,
+			String baseUrl) {
+		api.setType(new Type("Comment",
+				networkInfo.getHostName(),
+				networkInfo.getId()+"_Comment"));
+		api.setId(entity.getCommentId());
+		api.setDescription(entity.getBody());
+		
+		api.setInReplyToUri(entity.getPost().getUri());
+		api.setInReplyToUrl(baseUrl+entity.getPost().getUri());
+		
+		populatePostProperties(api, entity);
+		return api;
+	}
+	
+	abstract protected void populatePostProperties(InternalTimelineElement api, C entity);
+
+}
--- src/main/java/net/mixednutz/app/server/repository/CommentRepository.java
+++ src/main/java/net/mixednutz/app/server/repository/CommentRepository.java
@@ -1,11 +1,52 @@
 package net.mixednutz.app.server.repository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 
+import net.mixednutz.app.server.entity.User;
 import net.mixednutz.app.server.entity.post.AbstractPostComment;
 
 @NoRepositoryBean
 public interface CommentRepository<C extends AbstractPostComment> extends CrudRepository<C, Long> {
 
+	List<C> findByAuthorAndDateCreatedGreaterThanOrderByDateCreatedDesc(User author, ZonedDateTime dateCreated, Pageable pageRequest);
+	
+	default List<C> getMyPostsGreaterThan(User author, ZonedDateTime dateCreated, Pageable pageRequest) {
+		return findByAuthorAndDateCreatedGreaterThanOrderByDateCreatedDesc(author, dateCreated, pageRequest);
+	}
+	
+	List<C> findByAuthorAndDateCreatedLessThanEqualOrderByDateCreatedDesc(User owner, ZonedDateTime datePublished, Pageable pageRequest);
+	
+	default List<C> getMyPostsLessThan(User author, ZonedDateTime dateCreated, Pageable pageRequest) {
+		return findByAuthorAndDateCreatedLessThanEqualOrderByDateCreatedDesc(author, dateCreated, pageRequest);
+	}
+	/*
+	 * SUBCLASS MUST DEFINE a @Query
+	 */
+	
+	List<C> queryAuthorPostsByDateCreatedGreaterThan(
+			Long authorId, 
+			Long viewerId, 
+			ZonedDateTime dateCreated, Pageable pageRequest);
+	
+	default List<C> getAuthorPostsByDateCreatedGreaterThan(User owner, User viewer, ZonedDateTime datePublished, Pageable pageRequest) {
+		return queryAuthorPostsByDateCreatedGreaterThan(owner.getUserId(), viewer!=null?viewer.getUserId():null, datePublished, pageRequest);
+	}
+	
+	/*
+	 * SUBCLASS MUST DEFINE a @Query
+	 */
+	List<C> queryAuthorPostsByDateCreatedLessThanEquals(
+			@Param("authorId")Long authorId, 
+			@Param("viewerId")Long viewerId, 
+			@Param("dateCreated")ZonedDateTime dateCreated, Pageable pageRequest);
+	
+	default List<C> getAuthorPostsByDatePublishedLessThanEquals(User owner, User viewer, ZonedDateTime datePublished, Pageable pageRequest) {
+		return queryAuthorPostsByDateCreatedLessThanEquals(owner.getUserId(), viewer!=null?viewer.getUserId():null, datePublished, pageRequest);
+	}
 }
