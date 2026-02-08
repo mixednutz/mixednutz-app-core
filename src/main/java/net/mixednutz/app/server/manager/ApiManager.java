5121c9567e6ef79de272d1e76f2c7dec2c02c3a6
Andy <apfes1@gmail.com>
2026-02-07 22:08:58 -0500

    added ability to convert visibility to api interface

--- src/main/java/net/mixednutz/app/server/manager/ApiManager.java
+++ src/main/java/net/mixednutz/app/server/manager/ApiManager.java
@@ -6,12 +6,16 @@ import org.springframework.security.core.Authentication;
 
 import net.mixednutz.api.model.IUser;
 import net.mixednutz.api.model.IUserSmall;
+import net.mixednutz.api.model.IVisibility;
 import net.mixednutz.app.server.entity.InternalTimelineElement;
 import net.mixednutz.app.server.entity.Oembeds.Oembed;
 import net.mixednutz.app.server.entity.User;
 import net.mixednutz.app.server.entity.UserProfile;
+import net.mixednutz.app.server.entity.Visibility;
 
 public interface ApiManager {
+	
+	public IVisibility toVisibility(Visibility visibility);
 	
 	public String getAvatarUri(String avatarFilename);
 
